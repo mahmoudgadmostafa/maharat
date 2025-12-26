61c450c78db590b64cd489ef3e316a9a75120067
ا/محمود جاد مصطفي <mahmoudgadmostafa@gmail.com>
2025-12-26 21:51:05 +0200

    Update StudentDashboard.jsx

--- src/components/StudentDashboard.jsx
+++ src/components/StudentDashboard.jsx
@@ -279,7 +279,7 @@ const StudentDashboard = memo(() => {
         <StudentHeader userData={userData} onLogout={logout} />
       </Suspense>
 
-      <div className="container mx-auto px-2 sm:px-4 py-8">
+      <div className="container mx-auto px-4 py-8">
         {showMessaging ? (
           <Suspense fallback={<LoadingSpinner />}>
             <StudentMessaging />
